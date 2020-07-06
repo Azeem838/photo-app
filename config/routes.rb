2b7cf736790a74b70c6c8b5e7adec2fba01d0b46
Azeem <azeemahmed101@gmail.com>
2020-07-06 07:51:29 +0200

    add email functionality for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
